42ee8b9d403b50a4fd472f07f42562cf18625f8e
ali-mohtashim <35138203+ali-mohtashim@users.noreply.github.com>
2021-04-14 16:29:32 +0500

    create role UI

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -1,54 +1,67 @@
 @extends('layouts.app')
 
-
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create New Role</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
 
-
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        </ul>
-    </div>
-@endif
-
-
-{!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Permission:</strong>
-            <br/>
-            @foreach($permission as $value)
-                <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
-                {{ $value->name }}</label>
-            <br/>
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
             @endforeach
+            </ul>
         </div>
+    @endif
+
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="card-title">
+                        <h5>Create New Role</h5>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="float-right">
+                        <a class="btn btn-info btn-sm" href="{{ route('roles.index') }}"> Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
+        <div class="card-body">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Name:</strong>
+                        {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Permission:</strong>
+                        <br/>
+                        <div class="row">
+                            @foreach($permission as $value)
+                                <div class="col-lg-3">
+                                    <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
+                                    {{ $value->name }}</label>
+                                </div>
+                            <br/>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-footer">
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-success btn-sm">Submit</button>
+            </div>
+        </div>    
+        {!! Form::close() !!}
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</div>
-{!! Form::close() !!}
 
 @endsection
